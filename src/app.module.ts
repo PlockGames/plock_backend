5e94f0825b0275e2324099c7f8bda1121bd65f76
Miquel Maxime <maxime.miquel@epitech.eu>
2024-05-27 10:12:40 +0200

    chore: Update npm dependencies and add AWS S3 client

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GamesController } from './games/games.controller';
+import { R2Service } from './r2/r2.service';
 
 @Module({
   imports: [],
   controllers: [AppController, GamesController],
-  providers: [AppService],
+  providers: [AppService, R2Service],
 })
 export class AppModule {}
