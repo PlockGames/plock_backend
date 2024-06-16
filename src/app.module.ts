5200ffc2a31627e3b5807c06e0c88518cfa86ebe
pebloop <pebloop@outlook.com>
2024-06-16 23:03:38 +0200

    Fix backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { ConfigModule } from '@nestjs/config';
 import { GamesController } from './games/games.controller';
 import { R2Service } from './services/r2/r2.service';
 import { CommentsController } from './comments/comments.controller';
-import { WinConditionsController } from './winconditions/winconditions.controller';
-import { GameObjectsController } from './gameobjects/gameobjects.controller';
 
 @Module({
   imports: [
@@ -18,8 +16,6 @@ import { GameObjectsController } from './gameobjects/gameobjects.controller';
     AppController,
     GamesController,
     CommentsController,
-    WinConditionsController,
-    GameObjectsController,
   ],
   providers: [AppService, R2Service],
 })
