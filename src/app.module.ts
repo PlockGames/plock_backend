2ff37908ac9acdb22acea3b7ac21f3be2a31679d
soymustamahti <mustaelhachmimahti@gmail.com>
2024-07-22 23:52:22 +0200

    feat: remove unused app.* files, added user schema, and user structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { GamesController } from './games/games.controller';
 import { R2Service } from './services/r2/r2.service';
 import { CommentsController } from './comments/comments.controller';
 import { WinConditionsController } from './winconditions/winconditions.controller';
 import { GameObjectsController } from './gameobjects/gameobjects.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Make ConfigModule globally available
     }),
+    UserModule,
   ],
   controllers: [
-    AppController,
     GamesController,
     CommentsController,
     WinConditionsController,
     GameObjectsController,
   ],
-  providers: [AppService, R2Service],
+  providers: [R2Service],
 })
 export class AppModule {}
