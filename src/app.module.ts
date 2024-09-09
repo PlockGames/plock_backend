5b2644017e50b4d7af69b686fbe509eead5c817c
Mustapha <mustaelhachmimahti@gmail.com>
2024-09-09 22:19:12 +0200

    feat: add all entitis

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,28 @@ import { R2Module } from './r2/r2module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwt.guards';
 import { UserTypeGuard } from './auth/guards/user-type.guard';
+import { FriendModule } from './friend/friend.module';
+import { PostModule } from './post/post.module';
+import { MediaModule } from './media/media.module';
+import { CommentModule } from './comment/comment.module';
+import { GameModule } from './game/game.module';
+import { TagModule } from './tag/tag.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, AuthModule, R2Module],
+  imports: [
+    UserModule,
+    PrismaModule,
+    AuthModule,
+    R2Module,
+    FriendModule,
+    PostModule,
+    MediaModule,
+    CommentModule,
+    GameModule,
+    TagModule,
+    ReviewModule,
+  ],
   providers: [
     {
       provide: APP_GUARD,
