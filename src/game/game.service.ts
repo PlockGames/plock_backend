ed4396e29d44939ba3ebde62ffd3d06696f23aa1
pebloop <pebloop@outlook.com>
2025-04-12 23:06:21 +0200

    Debug: check user in get game

--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -83,7 +83,6 @@ export class GameService {
     if (!game) this.logger.warn(`Game with ID: ${id} not found`);
     this.logger.log(`Game retrieved: ${JSON.stringify(game)}`);
 
-    this.logger.log(`Checking if user ${user} has liked the game`);
     const hasLiked = await this.likeService.hasLikedGame(user?.id, id);
     this.logger.log(`User ${user?.id} has liked the game: ${hasLiked}`);
     return {
